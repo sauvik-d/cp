fc4985f6d3de595f0b8d7f542444d63ec56e9f0b
sauvik-d <sauvikdutta.nnhs.kol.e2720@gmail.com>
2023-10-16 02:03:25 +0530

    ..

---
+++ Codeforces/1881ADon'tTrytoCount.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define char_alloc(n) (char*)malloc(sizeof(char)*n)
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int n;
+    int m;
+    cin>>n;
+    cin>>m;
+    char* x = char_alloc(n);
+    char* s = char_alloc(m);
+    cin>>x;
+    cin>>s;
+    cout<<x<<" "<<s;
+    return 0;
+}
