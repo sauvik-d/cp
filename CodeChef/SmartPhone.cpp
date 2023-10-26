5e48493d2da0dee3a550eb4c0ae3bf8351615e97
sauvik-d <sauvikdutta.nnhs.kol.e2720@gmail.com>
2023-10-27 01:24:31 +0530

    ..

---
+++ CodeChef/SmartPhone.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    vector<long long> arr(n);
+    for(long long i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    long long maximum = 0;
+    long long count = 0;
+    sort(arr.begin(), arr.end());
+    for(long long i = 0; i<n;i++){
+        count = (n-i)*arr[i];
+        maximum = max(count, maximum);
+    }
+    cout<<maximum<<endl;
+    
+    return 0;
+}
