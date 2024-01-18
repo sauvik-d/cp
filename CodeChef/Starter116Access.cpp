fbdfb8bfc194c03ba6ced0f529d10b041c138abe
sauvik-d <sauvikdutta.nnhs.kol.e2720@gmail.com>
2024-01-18 20:40:21 +0530

    Got a runtime error in S116Access

---
+++ CodeChef/Starter116Access.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	int x, n;
+	while(t--){
+		cin>>n;
+		cin>>x;
+		string str;
+		for(int i=0;i<n;i++){
+			cin>>str[i];
+		}
+		int swipe = 0;
+		int flag = 0;
+		for(int i=0;i<n;i++){
+			if(str[i] == '0' && swipe != 0){
+				swipe--;
+			}
+			else if(str[i] == '1'){
+				swipe = x;
+			}
+			else{
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 1){
+			cout<<"NO\n";
+		}
+		else{
+			cout<<"YES\n";
+		}
+	}
+}
